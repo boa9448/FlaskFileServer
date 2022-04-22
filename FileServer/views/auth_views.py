b6ce62fbe6a460fc09c8857634083af849486dd9
WDW <boa3465@gmail.com>
2022-04-23 03:42:53 +0900

    로그인체크, 로그아웃 구현

--- FileServer/views/auth_views.py
+++ FileServer/views/auth_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, url_for, session
+from flask import Blueprint, render_template, request, url_for, session, g, flash
 from werkzeug.utils import redirect
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -15,7 +15,8 @@ bp = Blueprint("auth", __name__, url_prefix="/auth")
 def index():
     return redirect("login")
 
-@bp.route("/login", methods = ("GET", "POST"))
+
+@bp.route("/login/", methods = ("GET", "POST"))
 def login():
     form = LoginForm()
     if request.method == "POST" and form.validate_on_submit():
@@ -23,15 +24,28 @@ def login():
         user = User().query.filter_by(username = form.username.data).first()
         if not user:
             error = "유저가 없음"
-            return "유저가 없음"
-        #elif not check_password_hash(user.password, form.password.data):
-        if not user.password == form.password.data:
+        elif not check_password_hash(user.password, form.password.data):
             error = "비밀번호가 틀림"
-            return "비밀번호가 틀림"
         
         if error is None:
             session.clear()
             session["user_id"] = user.id
-            return "로그인 성공!"
+            return redirect(url_for("main.index"))
 
+        flash(error)
     return render_template("auth/login.html", form = form)
+
+
+@bp.route("/logout/")
+def logout():
+    session.clear()
+    return redirect("login")
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get("user_id")
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = User.query.get(user_id)
--- FileServer/views/main_views.py
+++ FileServer/views/main_views.py
@@ -1,7 +1,11 @@
-from flask import Blueprint
+from flask import Blueprint, url_for, g, session
+from werkzeug.utils import redirect
 
 bp = Blueprint("main", __name__, url_prefix = "/")
 
 @bp.route("/")
 def index():
-    return "메인 페이지"
+    if not g.user:
+        return redirect(url_for("auth.login"))
+
+    return "파일 리스트"
