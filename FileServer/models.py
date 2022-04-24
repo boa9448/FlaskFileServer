1e00b5a799141b5f3e34b8789522bd64ffbb047f
WDW <boa3465@gmail.com>
2022-04-24 09:15:36 +0900

    컬럼명 변경

--- FileServer/models.py
+++ FileServer/models.py
@@ -6,7 +6,7 @@ class User(db.Model):
     username = db.Column(db.String(150), unique = True, nullable = False)
     password = db.Column(db.String(200), nullable = False)
     email = db.Column(db.String(120), unique = True, nullable = False)
-    admin_permission = db.Column(db.Integer, nullable = False)
+    admin_permission = db.Column(db.Integer, nullable = False, default = 1)
     permission = db.Column(db.Integer, nullable = False)
     create_date = db.Column(db.DateTime(), nullable=False)
 
@@ -14,6 +14,7 @@ class User(db.Model):
 class File(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     filename = db.Column(db.String(200), nullable = False)
-    filehash = db.Column(db.String(200), nullable = False)
+    hash = db.Column(db.String(200), nullable = False)
+    size = db.Column(db.Integer, nullable = False)
     permission = db.Column(db.Integer, nullable = False)
     
