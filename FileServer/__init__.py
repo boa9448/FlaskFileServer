053b67c6190c6c21b5360c689e7dac6cea46fb06
wdw <boa3465@gmail.com>
2022-04-25 16:25:15 +0900

    sqlite 네이밍 문제 수정

--- FileServer/__init__.py
+++ FileServer/__init__.py
@@ -1,9 +1,16 @@
 from flask import Flask, render_template
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 
-
-db = SQLAlchemy()
+naming_convention = {
+    "ix": 'ix_%(column_0_label)s',
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(column_0_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s"
+}
+db = SQLAlchemy(metadata=MetaData(naming_convention=naming_convention))
 migrate = Migrate()
 
 
@@ -15,10 +22,14 @@ def create_app():
     app = Flask(__name__)
     app.config.from_envvar("APP_CONFIG_FILE")
 
+    # ORM
     db.init_app(app)
-    migrate.init_app(app, db)
-    from . import models
+    if app.config['SQLALCHEMY_DATABASE_URI'].startswith("sqlite"):
+        migrate.init_app(app, db, render_as_batch=True)
+    else:
+        migrate.init_app(app, db)
 
+    from . import models
     from .views import main_views, auth_views, file_views
     app.register_error_handler(404, page_not_found)
     app.register_blueprint(main_views.bp)
